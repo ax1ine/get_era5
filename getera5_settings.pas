0d1943c54c5742d92240b536e6b4c661063d4591
axline <axline@mail.ru>
2020-11-30 21:16:32 +0700

    settings updated

--- getera5_settings.pas
+++ getera5_settings.pas
@@ -5,24 +5,25 @@ unit getera5_settings;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, IniFiles;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, Spin,
+  IniFiles;
 
 type
 
   { Tfrmsettings }
 
   Tfrmsettings = class(TForm)
-    btnKey: TButton;
     btnPython: TButton;
     btnInstallPackages: TButton;
     eKey: TEdit;
     ePythonPath: TEdit;
     GroupBox1: TGroupBox;
+    GroupBox2: TGroupBox;
     GroupBox5: TGroupBox;
     Memo1: TMemo;
     OD: TOpenDialog;
+    seUID: TSpinEdit;
 
-    procedure btnKeyClick(Sender: TObject);
     procedure btnPythonClick(Sender: TObject);
     procedure btnInstallPackagesClick(Sender: TObject);
     procedure ePythonPathChange(Sender: TObject);
@@ -54,32 +55,18 @@ Var
   st:string;
 begin
   Ini:=TIniFile.Create(IniFileName);
-    ePythonPath.Text:=Ini.ReadString('main', 'PythonPath',
-                      GlobalPath+'Python39'+PathDelim+'Python.exe');
+    ePythonPath.Text:=Ini.ReadString('main', 'PythonPath', '');
   Ini.Free;
 
   if FileExists(GetUserDir+'.cdsapirc') then begin
     AssignFile(dat, GetUserDir+'.cdsapirc'); reset(dat);
      readln(dat, st);
      readln(dat, st);
-     eKey.Text:=trim(copy(st, 5, length(st)));
+     st:=copy(st, 5, length(st));
+     seUID.Text:=trim(copy(st, 1, Pos(':', st)-1));
+     eKey.Text:=trim(copy(st, Pos(':', st)+1, length(st)));
     CloseFile(dat);
   end;
-end;
-
-procedure Tfrmsettings.btnKeyClick(Sender: TObject);
-Var
-  dat:text;
-begin
- if trim(eKey.Text)<>'' then begin
-  if FileExists(GetUserDir+'.cdsapirc') then
-   if MessageDlg(SFileExists, mtWarning, [mbYes, mbNo], 0)=mrNo then exit;
-
-   AssignFile(dat, GetUserDir+'.cdsapirc'); rewrite(dat);
-     writeln(dat, 'url: https://cds.climate.copernicus.eu/api/v2');
-     writeln(dat, 'key: '+eKey.Text);
-   CloseFile(dat);
- end;
 end;
 
 procedure Tfrmsettings.btnPythonClick(Sender: TObject);
@@ -103,10 +90,18 @@ end;
 procedure Tfrmsettings.SaveSettings;
 Var
   Ini:TIniFile;
+  dat:text;
 begin
   Ini:=TIniFile.Create(IniFileName);
   Ini.WriteString('main', 'PythonPath', ePythonPath.Text);
   Ini.Free;
+
+  if trim(eKey.Text)<>'' then begin
+    AssignFile(dat, GetUserDir+'.cdsapirc'); rewrite(dat);
+     writeln(dat, 'url: https://cds.climate.copernicus.eu/api/v2');
+     writeln(dat, 'key: '+seUID.Text+':'+eKey.Text);
+    CloseFile(dat);
+  end;
 end;
 
 procedure Tfrmsettings.btnInstallPackagesClick(Sender: TObject);
